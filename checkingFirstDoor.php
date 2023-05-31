0c991dad460871e6bcb8d5b28f9e05ffb2a5b6bd
c0mrate <88302292+c0mrate@users.noreply.github.com>
2023-05-31 14:50:30 +0700

    2

--- checkingFirstDoor.php
+++ checkingFirstDoor.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: index"); // Redirect to the login page if not logged in
+    header("Location: index.php"); // Redirect to the login page if not logged in
     exit();
 }
 ?>
--- password.php
+++ password.php
@@ -1,5 +1,5 @@
 <?php
-require_once('checkingFirstDoor');
+require_once('checkingFirstDoor.php');
 ?>
 
 <!DOCTYPE html>
