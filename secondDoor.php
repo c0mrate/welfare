b10c5599e950ea1bc8e0cc38e31ae40fd4cbd767
c0mrate <88302292+c0mrate@users.noreply.github.com>
2023-05-31 15:10:12 +0700

    change file name

--- secondDoor.php
+++ secondDoor.php
@@ -4,7 +4,7 @@ session_start();
 // Validate login credentials (you should perform proper validation and authentication here)
 if ($_POST['contents'] === 'root') {
     $_SESSION['logged_in'] = true;
-    header("Location: web/#"); // Redirect to the second page
+    header("Location: web/store"); // Redirect to the second page
     exit();
 } else {
     echo '<script>window.history.go(-2);</script>';
