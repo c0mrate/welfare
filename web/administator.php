772d3d3a14ec5a19a4b60800e31dee99fb0c1f66
c0mrate <88302292+c0mrate@users.noreply.github.com>
2023-06-13 16:16:26 +0700

    create admin panel

--- web/administator.php
+++ web/administator.php
@@ -3,7 +3,6 @@ session_start();
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
 }
-
 if (!isset($_SESSION['username'])) {
     header("Location: account/login.html");
     exit();
